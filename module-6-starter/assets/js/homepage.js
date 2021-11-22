b4a8efd11873eb6f55c57899660ce0bd22084407
Bun Pape <benjaminhpage@me.com>
2021-11-22 18:38:06 -0500

    added error if statements to fetch API

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -21,13 +21,26 @@ var getUserRepos = function(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      displayRespos(data, user);
-    });
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRespos(data, user);
+      });
+    } else {
+      alert("Error:GitHub User Not Found");
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to GitHub");
   });
 };
 
 var displayRespos = function(repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  };
+
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
 
